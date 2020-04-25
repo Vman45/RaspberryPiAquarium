b7bbe68064bfae7a49538509a897a1616df0ed52
RatSteak <ratsteak@protonmail.com>
2020-04-25 12:33:34 +0200

    Python controller

---
+++ Python/controller.py
@@ -0,0 +1,12 @@
+#!/bin/python
+import RPi.GPIO as GPIO
+import sys
+
+GPIO.setmode(GPIO.BCM)
+RELAY=17
+GPIO.setup(RELAY,GPIO.OUT)
+
+if sys.argv[1] == "on":
+    GPIO.output(RELAY,GPIO.HIGH)
+else:
+    GPIO.output(RELAY,GPIO.LOW)
